9784734de6bc21dd93d015c7b46c1a6b57a25ea1
DURUNB <durunb@192.168.1.8>
2025-03-30 14:03:57 +0300

    Initial commit

---
+++ Products.js
@@ -0,0 +1,95 @@
+console.log("products.js loaded successfully!");
+var products = [
+    {
+        "id": 1,
+        "title": "Majestic Mountain Graphic T-Shirt",
+        "description": "Stylish black t-shirt featuring a striking monochrome mountain range graphic.",
+        "category": "clothes",
+        "price": 44,
+        "images": ["https://i.imgur.com/QkIa5tT.jpeg", "https://i.imgur.com/jb5Yu0h.jpeg", "https://i.imgur.com/UlxxXyG.jpeg"],
+        "thumbnail": "https://i.imgur.com/QkIa5tT.jpeg"
+    },
+    {
+        "id": 2,
+        "title": "Eyeshadow Palette with Mirror",
+        "description": "Versatile eyeshadow palette with a built-in mirror for easy application.",
+        "category": "beauty",
+        "price": 19.99,
+        "images": ["https://cdn.dummyjson.com/products/images/beauty/Eyeshadow%20Palette%20with%20Mirror/1.png"],
+        "thumbnail": "https://cdn.dummyjson.com/products/images/beauty/Eyeshadow%20Palette%20with%20Mirror/thumbnail.png"
+    },
+    {
+        "id": 3,
+        "title": "Red Nail Polish",
+        "description": "Glossy red nail polish with a quick-drying formula for a salon-quality finish.",
+        "category": "beauty",
+        "price": 8.99,
+        "images": ["https://cdn.dummyjson.com/products/images/beauty/Red%20Nail%20Polish/1.png"],
+        "thumbnail": "https://cdn.dummyjson.com/products/images/beauty/Red%20Nail%20Polish/thumbnail.png"
+    },
+    {
+        "id": 4,
+        "title": "Chanel Coco Noir Eau De",
+        "description": "Elegant fragrance with notes of grapefruit, rose, and sandalwood.",
+        "category": "fragrances",
+        "price": 129.99,
+        "images": ["https://cdn.dummyjson.com/products/images/fragrances/Chanel%20Coco%20Noir%20Eau%20De/1.png"],
+        "thumbnail": "https://cdn.dummyjson.com/products/images/fragrances/Chanel%20Coco%20Noir%20Eau%20De/thumbnail.png"
+    },
+    {
+        "id": 5,
+        "title": "Classic Red Jogger Sweatpants",
+        "description": "Comfortable jogger sweatpants with adjustable drawstring and side pockets.",
+        "category": "clothes",
+        "price": 98,
+        "images": ["https://i.imgur.com/mp3rUty.jpeg", "https://i.imgur.com/JQRGIc2.jpeg"],
+        "thumbnail": "https://i.imgur.com/mp3rUty.jpeg"
+    },
+    {
+        "id": 6,
+        "title": "Cat Food",
+        "description": "Nutritious cat food formulated for a balanced feline diet.",
+        "category": "groceries",
+        "price": 8.99,
+        "images": ["https://cdn.dummyjson.com/products/images/groceries/Cat%20Food/1.png"],
+        "thumbnail": "https://cdn.dummyjson.com/products/images/groceries/Cat%20Food/thumbnail.png"
+    },
+    {
+        "id": 7,
+        "title": "Chicken Meat",
+        "description": "Fresh and tender chicken meat for various culinary dishes.",
+        "category": "groceries",
+        "price": 9.99,
+        "images": ["https://cdn.dummyjson.com/products/images/groceries/Chicken%20Meat/1.png"],
+        "thumbnail": "https://cdn.dummyjson.com/products/images/groceries/Chicken%20Meat/thumbnail.png"
+    },
+    {
+        "id": 8,
+        "title": "Juice",
+        "description": "Refreshing fruit juice, packed with vitamins and great for hydration.",
+        "category": "groceries",
+        "price": 3.99,
+        "images": ["https://cdn.dummyjson.com/products/images/groceries/Juice/1.png"],
+        "thumbnail": "https://cdn.dummyjson.com/products/images/groceries/Juice/thumbnail.png"
+    },
+    {
+        "id": 9,
+        "title": "Kiwi",
+        "description": "Nutrient-rich kiwi, perfect for snacking or adding a tropical twist.",
+        "category": "groceries",
+        "price": 2.49,
+        "images": ["https://cdn.dummyjson.com/products/images/groceries/Kiwi/1.png"],
+        "thumbnail": "https://cdn.dummyjson.com/products/images/groceries/Kiwi/thumbnail.png"
+    },
+    {
+        "id": 10,
+        "title": "Sleek White & Orange Wireless Gaming Controller",
+        "description": "Elevate your gaming experience with this state-of-the-art wireless controller, featuring a crisp white base with vibrant orange accents. Designed for precision play, the ergonomic shape and responsive buttons provide maximum comfort and control for endless hours of gameplay. Compatible with multiple gaming platforms, this controller is a must-have for any serious gamer looking to enhance their setup.",
+        "category": "electronics",
+        "price": 69,
+        "images": ["https://i.imgur.com/ZANVnHE.jpeg"],
+        "thumbnail": "https://i.imgur.com/ZANVnHE.jpeg"
+    }
+
+];
+export default products;
